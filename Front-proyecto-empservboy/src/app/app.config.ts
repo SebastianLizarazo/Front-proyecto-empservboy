4131a263b0dfe68efeba3482487f412861ee556f
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2025-11-23 12:10:33 -0500

    diseño de login y navbar terminado

--- Front-proyecto-empservboy/src/app/app.config.ts
+++ Front-proyecto-empservboy/src/app/app.config.ts
@@ -1,20 +1,3 @@
-/*
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-};
-*/
-
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
